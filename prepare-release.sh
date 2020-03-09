523396f92507a6a288dce32abb6a41fceb369839
gus-esmartit <gustavo.rodriguez@adevinta.com>
2020-03-09 23:41:47 +0100

    adding app

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # This is a comment!
 echo Hello World!!        # This is a comment, too!
+
+docker build -t esmartit/smartpoke-dashboard:"$1" -t esmartit/smartpoke-dashboard:latest
+                            .
