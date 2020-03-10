68625a5cfd1e90ca8efb9634389434e97b19c014
gus-esmartit <gustavo.rodriguez@adevinta.com>
2020-03-10 09:48:06 +0100

    feat: add a test file

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-docker build -t esmartit/smartpoke-dashboard:"$1" -t esmartit/smartpoke-dashboard:latest .
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker push esmartit/smartpoke-dashboard:"$1"
-docker push esmartit/smartpoke-dashboard:latest
-sed -i "" "s/x.x.x/$1/g" smartpoke-dashboard/Chart.yaml
+#docker build -t esmartit/smartpoke-dashboard:"$1" -t esmartit/smartpoke-dashboard:latest .
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
+#docker push esmartit/smartpoke-dashboard:"$1"
+#docker push esmartit/smartpoke-dashboard:latest
+sed -i "s/x.x.x/$1/g" "${PWD}/smartpoke-dashboard/Chart.yaml"
 helm package smartpoke-dashboard
 exit
