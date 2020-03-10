a1ed14610b19ff460adad38ef535e3afd5b74b04
gus-esmartit <gustavo.rodriguez@adevinta.com>
2020-03-10 01:46:53 +0100

    feat: add a test file

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-# This is a comment!
-echo Hello World!!        # This is a comment, too!
-
 docker build -t esmartit/smartpoke-dashboard:"$1" -t esmartit/smartpoke-dashboard:latest .
-                            .
+exit
