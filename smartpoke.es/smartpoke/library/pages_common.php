05248d8df3a1ad21d89cba322e4c1ae0fec8bc52
adolfozignago <adolfo.zignago@esmartit.es>
2020-06-02 16:59:03 +0200

    fix: include SMS certs

--- smartpoke.es/smartpoke/library/pages_common.php
+++ smartpoke.es/smartpoke/library/pages_common.php
@@ -639,43 +639,46 @@
   }
 
   function sendWorldLine($phone, $message, $sender) {
-  
-		//certificado pem extraido de un pkcs12 con la ruta completa absoluta
-		$cert = "https://srv.smartpoke.es/certs_sms/esmartit.pem";
 
-		//password del certificado pem
-		$passwd = "QsRam4C6";
+//  certificado pem extraido de un pkcs12 con la ruta completa absoluta
+    $cert = "/certs_sms/esmartit.pem";
+    $key = "/certs_sms/esmartit_key.pem";
+
+//  password del certificado pem
+    $passwd = "Te2pp2so";
 
     $param='user=ESMARTIT'.
-			'&company=ESMARTIT'.
-			'&passwd=P45_m61X'.
-			'&gsm=%2B'.$phone.
-			'&type=plus'.
-	    '&msg='.$message.
-	    '&sender='.$sender;
+            '&company=ESMARTIT'.
+            '&passwd=P45_m61X'.
+            '&gsm=%2B'.$phone.
+            '&type=plus'.
+            '&msg='.$message.
+            '&sender='.$sender;
 
-  	$url = 'https://push.tempos21.com/mdirectnx-trust/send?';	 
+//    $url = 'https://push.tempos21.com/mdirectnx-trust/send?'; Ruta con IP
+    $url = 'https://push.tempos21.com/mdirectnx/send?';
     $ch = curl_init();
-    
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+//  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//  curl_setopt($ch, CURLOPT_URL, $url);
+//  curl_setopt($ch, CURLOPT_POST, 1);
+//  curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+
     curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSLVERSION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSLCERT, $cert);
+    curl_setopt($ch, CURLOPT_SSLKEY, $key);
+    curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $passwd);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
 
-		//     curl_setopt($ch, CURLOPT_URL, $url);
-		// curl_setopt ($ch,CURLOPT_SSLVERSION, 3);
-		// curl_setopt ($ch,CURLOPT_SSL_VERIFYPEER,false);
-		// curl_setopt ($ch,CURLOPT_SSL_VERIFYHOST, 0);
-		// curl_setopt ($ch,CURLOPT_SSLCERT, $cert);
-		// curl_setopt ($ch,CURLOPT_SSLCERTPASSWD,$passwd);
-		//     curl_setopt($ch, CURLOPT_POST, 1);
-		//     curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
-    
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $output = curl_exec($ch);
-     
+
     if (curl_errno($ch)) {
-      $output = 'Error:' . curl_error($ch);
+        $output = 'Error:' . curl_error($ch);
     }
     curl_close($ch);
 
